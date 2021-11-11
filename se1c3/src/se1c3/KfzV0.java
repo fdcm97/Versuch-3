3ac33d81c42e5af66da3a2fa98880ddbb15698b8
fdcm97 <94042493+fdcm97@users.noreply.github.com>
2021-11-11 15:31:32 +0100

    Add files via upload

---
+++ se1c3/src/se1c3/KfzV0.java
@@ -0,0 +1,32 @@
+package se1c3;
+
+public class KfzV0 {
+	private int sitze;
+	private int tankInhalt;
+	public float verbrauch;
+	
+	public KfzV0(int sitze, int tankInhalt, float verbrauch) {
+		
+		this.sitze = sitze;
+		this.tankInhalt = tankInhalt;
+		this .verbrauch = verbrauch;
+	}
+	
+	public float reichweite() {
+		return(tankInhalt*100)/verbrauch;
+	}
+	
+	public float spritVerbrauch(int km) {
+		return(km*verbrauch)/100;
+	}
+	
+	public class Kfz {
+		
+		private Kfz(int sitze, int tankInhalt, float verbrauch){
+						
+		}
+
+
+	}
+
+}
